fe9fcc578f5ffe99679dd44b87557a55f18b5433
Alex <voronin.adm@mail.ru>
2022-03-16 14:15:03 +0300

    form functions refactoring

--- js/form.js
+++ js/form.js
@@ -6,21 +6,14 @@ const mainFormFieldsets = mainForm.querySelectorAll('fieldset');
 const mainFormSlider = mainForm.querySelector('.ad-form__slider');
 const mapFiltersElements = mapFilters.querySelectorAll('select, fieldset');
 
-const toggleFormToUnactive = (isActive) => {
-  mainForm.classList.toggle('ad-form--disabled', isActive);
-  mapFilters.classList.toggle('map__filters--disabled', isActive);
-  mainFormFieldsets.forEach((element) => {
-    element.disabled = isActive;
-    element.children.disabled = isActive;
-  });
-  mainFormSlider.classList.toggle('ad-form--disabled', isActive);
-  mapFiltersElements.forEach((element) => {
-    element.disabled = isActive;
-    element.children.disabled = isActive;
-  });
-};
+const priceField = mainForm.querySelector('[name="price"]');
+const typeOfHousesField = mainForm.querySelector('[name="type"]');
+const timeIn = mainForm.querySelector('[name="timein"]');
+const timeOut = mainForm.querySelector('[name="timeout"]');
+const timeInOutParent = mainForm.querySelector('.ad-form__element--time');
+const rooms = mainForm.querySelector('[name="rooms"]');
+const capacity = mainForm.querySelector('[name="capacity"]');
 
-//FORM VALIDATING
 const pristine = new Pristine(mainForm, {
   classTo: 'ad-form__element--validating',
   errorClass: 'ad-form__element--validating-danger',
@@ -30,68 +23,73 @@ const pristine = new Pristine(mainForm, {
   errorTextClass: 'ad-form__element--validating-error'
 });
 
-//title validation
+const initForm = (isActive) => {
 
-function validateTitle(value) {
-  return value.length >= 30 && value.length <= 100;
-}
+  //=======FORM DISABLING-ACTIVATING
+  const toggleFormToUnactive = () => {
+    mainForm.classList.toggle('ad-form--disabled', isActive);
+    mapFilters.classList.toggle('map__filters--disabled', isActive);
+    mainFormFieldsets.forEach((element) => {
+      element.disabled = isActive;
+      element.children.disabled = isActive;
+    });
+    mainFormSlider.classList.toggle('ad-form--disabled', isActive);
+    mapFiltersElements.forEach((element) => {
+      element.disabled = isActive;
+      element.children.disabled = isActive;
+    });
 
-pristine.addValidator(mainForm.querySelector('[name="title"]'),
-  validateTitle,
-  'Заголовок должен быть не меньше 30 и не более 100 символов', 1, false);
+    //=======FORM VALIDATING
+    function validateTitle(value) {
+      return value.length >= 30 && value.length <= 100;
+    }
+    function onLivingTypeChange() {
+      priceField.placeholder = MIN_HOUSING_PRICES[this.value];
+      pristine.validate(priceField);
+    }
+    function validatePrice(value) {
+      return value >= MIN_HOUSING_PRICES[typeOfHousesField.value] && value <= 100000;
+    }
+    function getPriceErrorMessage() {
+      return `Не менее ${MIN_HOUSING_PRICES[typeOfHousesField.value]} и не более 100 000`;
+    }
+    function validateCapacity() {
+      return ROOMS_CAPACITY[rooms.value].includes(capacity.value);
+    }
 
-//handler. synchronize type of houses and min price
+    if (!isActive) {
+      //title validation
+      pristine.addValidator(mainForm.querySelector('[name="title"]'),
+        validateTitle,
+        'Заголовок должен быть не меньше 30 и не более 100 символов', 1, false);
 
-const priceField = mainForm.querySelector('[name="price"]');
-const typeOfHousesField = mainForm.querySelector('[name="type"]');
+      //handler. synchronize type of houses and min price
+      typeOfHousesField.addEventListener('change', onLivingTypeChange);
 
-function onLivingTypeChange() {
-  priceField.placeholder = MIN_HOUSING_PRICES[this.value];
-  pristine.validate(priceField);
-}
-typeOfHousesField.addEventListener('change', onLivingTypeChange);
+      //price for living validation
+      pristine.addValidator(priceField, validatePrice, getPriceErrorMessage, 1, false);
 
-//price for living validation
+      //handler. synchronize checkin and checkout
+      timeInOutParent.addEventListener('change', (evt) => {
+        timeIn.value = timeOut.value = evt.target.value;
+      });
 
-function validatePrice(value) {
-  return value >= MIN_HOUSING_PRICES[typeOfHousesField.value] && value <= 100000;
-}
+      //synchronize rooms and capacity
+      pristine.addValidator(capacity, validateCapacity, 'Пожалуйста, выберите верное количество гостей или комнат', 1, false);
 
-function getPriceErrorMessage() {
-  return `Не менее ${MIN_HOUSING_PRICES[typeOfHousesField.value]} и не более 100 000`;
-}
+      rooms.addEventListener('change', () => {
+        pristine.validate(capacity);
+      });
 
-pristine.addValidator(priceField, validatePrice, getPriceErrorMessage, 1, false);
-
-//handler. synchronize checkin and checkout
-
-const timeIn = mainForm.querySelector('[name="timein"]');
-const timeOut = mainForm.querySelector('[name="timeout"]');
-const timeInOutParent = mainForm.querySelector('.ad-form__element--time');
-
-timeInOutParent.addEventListener('change', (evt) => {
-  timeIn.value = timeOut.value = evt.target.value;
-});
-
-//synchronize rooms and capacity
-
-const rooms = mainForm.querySelector('[name="rooms"]');
-const capacity = mainForm.querySelector('[name="capacity"]');
-
-function validateCapacity() {
-  return ROOMS_CAPACITY[rooms.value].includes(capacity.value);
-}
-
-pristine.addValidator(capacity, validateCapacity, 'Пожалуйста, выберите верное количество гостей или комнат', 1, false);
-
-rooms.addEventListener('change', () => {
-  pristine.validate(capacity);
-});
-
-mainForm.addEventListener('submit', (evt) => {
-  evt.preventDefault();
-  pristine.validate();
-});
+      //handler. form validating on submit
+      mainForm.addEventListener('submit', (evt) => {
+        evt.preventDefault();
+        pristine.validate();
+      });
+    }
+  };
+  toggleFormToUnactive();
+};
 
 
-export { toggleFormToUnactive };
+export { initForm };
